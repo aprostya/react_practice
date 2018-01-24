14b37b9dde27f7b7ce0963faa5b686a80bbf1494
Shura <shura@MacBook-Air-x.local>
2018-01-24 22:30:20 +0300

    json data

--- src/App.js
+++ src/App.js
@@ -9,18 +9,28 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
-        }
-        render() {
+    }
+
+    render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
             return (
                 <div className="main-header">
                     <h1 className="main-header__title">CATS SEARCH</h1>
@@ -29,6 +39,7 @@ class App extends Component {
                 </div>
             );
         }
-    };
+    }
+};
 
-    export default App;
+export default App;
